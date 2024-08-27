35915fee2fc874e1b353f49c6a4f4d68ab3ce4e3
arijitbose <arijitbos@gmail.com>
2024-08-27 16:44:36 +0530

    Update main.py

--- cf1/main.py
+++ cf1/main.py
@@ -6,6 +6,7 @@ def GCSDataRead(event, context):
     #blobName = event['name']
     blobName = "hour.csv"
     fileName = "gs://" + bucketName + "/" + blobName
+    apiKey = "a1b2c33d4e5f6g7h8i9jakblc"
     
     dataFrame = pd.read_csv(fileName, sep=",")
     print(dataFrame.head(3))
